113f6b2b665328a0f71ce07e81ac1197848418fe
Marquixe <marquix.dev@gmail.com>
2026-04-11 21:39:33 +0200

    Hud, fixed first placing. Now you will grow forest

--- home/hypr/hud.sh
+++ home/hypr/hud.sh
@@ -5,7 +5,7 @@ SESSION="hud"
 ANIMATIONS=(
     "asciiquarium"
     "cmatrix -C cyan"
-    "cbonsai -l -i"
+    "cbonsai -l -i --life=46 --time=0,001"
     "pipes.sh -p 5 -R -C -K -r 0 -t 0 -t 1 -t 2 -t 3 -t 4 -t 5 -t 6 -t 7 -t 8 -t 9"
     "lavat -r 3"
     "snowmachine snow --speed=5" # --stack=pile
@@ -22,7 +22,15 @@ tmux new-session -d -s $SESSION -x 220 -y 98 "tty-clock -s -c -C 6 -f ''"
 # vertical stack
 DATE_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$SESSION:0.0" -v -l 80 "bash ~/.config/hypr/hud-date.sh")
 STATS_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$DATE_PANE" -v -l 65 "bash ~/.config/hypr/hud-stats.sh")
-ANIM_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$STATS_PANE" -v -l 45 "$PICK")
+#ANIM_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$STATS_PANE" -v -l 45 "$PICK")
+
+if [[ "$PICK" == "cbonsai -l -i --life=46 --time=0,001" ]]; then
+    ANIM_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$STATS_PANE" -v -l 45 "sleep 1; $PICK -s $RANDOM")
+    tmux split-window -d -t "$ANIM_PANE" -h -b -p 33 "sleep 1; $PICK -s $((RANDOM + 23))"
+    tmux split-window -d -t "$ANIM_PANE" -h -p 50 "sleep 1; $PICK -s $((RANDOM + 455))"
+else
+    tmux split-window -d -t "$STATS_PANE" -v -l 45 "$PICK"
+fi
 
 # split date row
 tmux split-window -d -t "$DATE_PANE" -h -b -p 33 ""
