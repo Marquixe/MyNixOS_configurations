4a08850418e18eb1edbd844f75198d8e4c7e3986
Marquixe <marquix.dev@gmail.com>
2026-04-11 20:42:02 +0200

    Hud, cleared v1

--- home/hypr/hud.sh
+++ home/hypr/hud.sh
@@ -16,19 +16,9 @@ PICK="${ANIMATIONS[$RANDOM%${#ANIMATIONS[@]}]}"
 
 tmux kill-session -t $SESSION 2>/dev/null
 sleep 0.2
-#tmux new-session -d -s $SESSION -x 220 -y 100 "tty-clock -s -c -C 6 -f ''"
-#tmux split-window -t $SESSION:0.0 -v -l 80 "bash ~/.config/hypr/hud-date.sh"
-#tmux split-window -t $SESSION:0.1 -v -l 65 "bash ~/.config/hypr/hud-stats.sh"
-#tmux split-window -t $SESSION:0.2 -h "gping google.com 1.1.1.1 8.8.8.8"
-#tmux split-window -t $SESSION:0.2 -v -l 45 "$PICK"
+tmux split-window -t $SESSION:0.2 -v -l 45 "$PICK"
 
 tmux new-session -d -s $SESSION -x 220 -y 98 "tty-clock -s -c -C 6 -f ''"
-#tmux split-window -t $SESSION:0.0 -v -l 80 "bash ~/.config/hypr/hud-date.sh"
-#tmux split-window -t $SESSION:0.1 -h ""
-#tmux split-window -t $SESSION:0.0 -v -l 65 "bash ~/.config/hypr/hud-stats.sh"
-#tmux split-window -t $SESSION:0.2 -v -l 45 "$PICK"
-#tmux split-window -t $SESSION:0.2 -h "gping google.com"
-
 # vertical stack
 DATE_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$SESSION:0.0" -v -l 80 "bash ~/.config/hypr/hud-date.sh")
 STATS_PANE=$(tmux split-window -dP -F "#{pane_id}" -t "$DATE_PANE" -v -l 65 "bash ~/.config/hypr/hud-stats.sh")
